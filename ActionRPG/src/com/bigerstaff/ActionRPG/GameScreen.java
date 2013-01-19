563051473d25834e19d12fe0e3fd617faa282778
David <d.bigerstaff@gmail.com>
2013-01-19 00:17:38 +0000

    Created Atlas using TexturePacker2 and used for animations.
    Took all night! I'm shit with Photoshop, Graphics Gale & Paint.NET ugh

--- ActionRPG/src/com/bigerstaff/ActionRPG/GameScreen.java
+++ ActionRPG/src/com/bigerstaff/ActionRPG/GameScreen.java
@@ -4,17 +4,26 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
 import com.badlogic.gdx.scenes.scene2d.ui.Touchpad.TouchpadStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.Array;
 
 public class GameScreen implements Screen {
+	ActionRPG game;
+	TextureAtlas atlas;
+	Animation playerAnimationWalkUp, playerAnimationWalkDown, playerAnimationWalkRight;
+	Animation playerAnimationSwordUp, playerAnimationSwordDown, playerAnimationSwordRight;
+	Array<Sprite> spriteWalkUp, spriteWalkDown, spriteWalkRight;
+	Array<Sprite> spriteSwordUp, spriteSwordDown, spriteSwordRight;
 	TextureRegion playerTextureRegion;
 	Texture playerTexture;
-	ActionRPG game;
 	Stage stage;
 	Touchpad touchpad;
 	TouchpadStyle touchpadStyle;
@@ -22,13 +31,33 @@ public class GameScreen implements Screen {
 	Drawable touchBackground;
 	Drawable touchKnob;
 	Player player;
+	float stateTime;
 	
    // constructor to keep a reference to the main Game class
     public GameScreen(ActionRPG tmpGame){
 		game = tmpGame;
+		
+		//Load sprite pack and load in to animations
+		atlas = new TextureAtlas(Gdx.files.internal("data/pack.atlas"));
+		spriteWalkUp = atlas.createSprites("walkup");
+		spriteWalkDown = atlas.createSprites("walkdown");
+		spriteWalkRight = atlas.createSprites("walkright");
+		spriteSwordUp = atlas.createSprites("swordup");
+		spriteSwordDown = atlas.createSprites("sworddown");
+		spriteSwordRight = atlas.createSprites("swordright");
+		playerAnimationWalkUp = new Animation(0.1f, spriteWalkUp);
+		playerAnimationWalkDown = new Animation(0.1f, spriteWalkDown);
+		playerAnimationWalkRight = new Animation(0.1f, spriteWalkRight);
+		playerAnimationSwordUp = new Animation(0.04f, spriteSwordUp);
+		playerAnimationSwordDown = new Animation(0.04f, spriteSwordDown);
+		playerAnimationSwordRight = new Animation(0.04f, spriteSwordRight);
+		
+		//Load player texture
 		playerTexture = new Texture(Gdx.files.internal("data/player.png"));
 		playerTextureRegion = new TextureRegion(playerTexture);
 		player = new Player(playerTexture);
+		//Load on screen touchpad
+		
 		touchpadSkin = new Skin();
 		touchpadSkin.add("touchBackground", new Texture("data/touchBackground.png"));
 		touchpadSkin.add("touchKnob", new Texture("data/touchKnob.png"));
@@ -43,13 +72,23 @@ public class GameScreen implements Screen {
     
     @Override
     public void render(float delta) {
+    	stateTime += Gdx.graphics.getDeltaTime();
     	Gdx.gl.glClearColor(0.294f, 0.294f, 0.294f, 1f);
         Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
         game.camera.update();
+
+        //Update player based on delta and X/Y of touchpad
         player.update(Gdx.graphics.getDeltaTime(), touchpad.getKnobPercentX(), touchpad.getKnobPercentY());
-        
+
+        //Draw
         game.spriteBatch.setProjectionMatrix(game.camera.combined);       
-        game.spriteBatch.begin();      
+        game.spriteBatch.begin();
+        game.spriteBatch.draw(playerAnimationWalkUp.getKeyFrame(stateTime, true), 500, 100);
+        game.spriteBatch.draw(playerAnimationWalkDown.getKeyFrame(stateTime, true), 500, 200);
+        game.spriteBatch.draw(playerAnimationWalkRight.getKeyFrame(stateTime, true), 500, 300);
+        game.spriteBatch.draw(playerAnimationSwordUp.getKeyFrame(stateTime, true), 600, 100);
+        game.spriteBatch.draw(playerAnimationSwordDown.getKeyFrame(stateTime, true), 600, 200);
+        game.spriteBatch.draw(playerAnimationSwordRight.getKeyFrame(stateTime, true), 600, 300);
         game.spriteBatch.draw(playerTexture, player.x, player.y);      	    
         game.spriteBatch.end();
 	    stage.act(Gdx.graphics.getDeltaTime());	    
